f5ef154c03e6e6a173cb0dec0019e84662608d80
xiao0803 <786876081@qq.com>
2018-09-29 23:53:59 +0800

    提交测试1

--- JAPDemo/spring-boot-jpa-demo/src/main/java/com/my/app/JpaApplication.java
+++ JAPDemo/spring-boot-jpa-demo/src/main/java/com/my/app/JpaApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author xiaolijun提交测1
+ */
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
